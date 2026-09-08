4741348caa7bc6c0a158117d458320c676cba01f
cosmos4ever-eva <Ruoyi.Wang0530@outlook.com>
2026-09-08 17:12:25 +0800

    feat: add balanced prime binary search review

---
+++ topics/06-binary-search/05-balanced-prime/original.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+vector<int> getPrime(int n){
+    vector<int> primes;
+    vector<int> isPrime(n+1,true);
+
+    for(int i = 2;i <= n;i ++){
+        if(isPrime[i]) {
+            primes.push_back(i);
+            for(int j = 2; j <= n/i ; j ++){
+                isPrime[i * j] = false;
+            }
+        }
+    }
+    return primes;
+}
+
+vector<int> getNeighborPrime(const vector<int>& primes){
+    vector<int> neighborprimes;
+
+    for(int i = 1;i < primes.size() - 1;i++){
+        if(primes[i] - primes[i-1] == primes[i+1] - primes[i]){
+            neighborprimes.push_back(primes[i]);
+        }
+    }
+    return neighborprimes;
+}
+
+int main(){
+    int T = 0;
+    cin >> T;
+    vector<int> N(T);
+    vector<int> primes = getPrime(10001000);
+    vector<int> neighborprimes = getNeighborPrime(primes);
+
+    for(int i = 0;i < T;i++){
+        cin >> N[i];
+        if(i != 0) cout << "\n";
+
+        auto it = lower_bound(neighborprimes.begin(),neighborprimes.end(),N[i]);
+        if(*it == N[i]){
+            cout << "Yes";
+        }
+        if(*it > N[i]){
+            cout << "No" << " " << *it;
+        }
+    }
+
+    return 0;
+}
