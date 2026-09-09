8a9f5b76bc442f45f951310e1ba5cb66966bd939
cosmos4ever-eva <Ruoyi.Wang0530@outlook.com>
2026-09-09 15:33:35 +0800

    docs: add alternative subset DFS solution

--- topics/07-depth-first-search/02-subsets-i/solution.cpp
+++ topics/07-depth-first-search/02-subsets-i/solution.cpp
@@ -1,38 +1,43 @@
-#include<iostream>
-#include<vector>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> result;
-vector<int> output;
-int n = 0;
+vector<vector<int> > result;
+vector<int> temp;
+int n;
 
-void DFS(int start,int depth){
-   if(depth == 0){
-       result.push_back(output);
-       return;
-   }
-
-   for(int j = start;j <= n - depth + 1;j++){
-       output.push_back(j);
-       DFS(j + 1,depth - 1);
-       output.pop_back();
-   }
+void DFS(int idx) {
+    if (idx == n + 1) {
+        result.push_back(temp);
+        return;
+    }
+    temp.push_back(idx);
+    DFS(idx + 1);
+    temp.pop_back();
+    DFS(idx + 1);
 }
 
-int main(){
-   cin >> n;
+bool cmp(vector<int> &a, vector<int> &b) {
+    if (a.size() != b.size()) {
+        return a.size() < b.size();
+    } else {
+        return a < b;
+    }
+}
 
-   for(int i = 0;i <= n;i++){
-       DFS(1,i);
-   }
-
-   for(int i = 0;i < result.size();i++){
-       if(i != 0) cout << "\n";
-       for(int j = 0;j < result[i].size();j++){
-           if(j != 0) cout << " ";
-           cout << result[i][j];
-       }
-   }
-
-   return 0;
+int main() {
+    scanf("%d", &n);
+    DFS(1);
+    sort(result.begin(), result.end(), cmp);
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = 0; j < result[i].size(); j++) {
+            printf("%d", result[i][j]);
+            if (j + 1 < result[i].size()) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
 }
